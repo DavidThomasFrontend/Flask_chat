cc25764c4b99c5ae54b0a1ef75d3dca7e7c07416
Vileblood <david.thomas1986@gmail.com>
2020-03-12 20:08:58 +0000

    added chat textbox function and refractored code

--- run.py
+++ run.py
@@ -27,9 +27,16 @@ def index():
 
     
 
-    @app.route('/username')
+    @app.route('/username', methods = ["GET", "POST"])
     def user(username):
         """Display chat messages"""
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form ["message"]
+        add_messages(username, message)
+        return redirect(session["username"]) #this is needed because everytime page reloads it will send same message again#
+
         return render_template("chat.html", username = username, chat_messages = messages)
 
     @app.route('/<username>/<message>')
